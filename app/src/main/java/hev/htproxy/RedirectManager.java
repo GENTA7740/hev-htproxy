79b4960398e50fa8a1f02a38ddc32ccec517ab79
Heiher <r@hev.cc>
2019-08-06 16:47:46 +0800

    RedirectManager: Dual stack support.

--- app/src/main/java/hev/htproxy/RedirectManager.java
+++ app/src/main/java/hev/htproxy/RedirectManager.java
@@ -5,6 +5,8 @@
 package hev.htproxy;
 
 import java.util.Set;
+import java.util.List;
+import java.util.ArrayList;
 import android.content.Context;
 
 public class RedirectManager {
@@ -13,10 +15,15 @@ public class RedirectManager {
 	private static final int TYPE_DELETE = 3;
 
 	private static final String cmd_iptables = "iptables -t nat ";
+	private static final String cmd_ip6tables = "ip6tables -t nat ";
 	private static final String netd_dnsproxy_path = "/dev/socket/dnsproxyd";
 
+	private static boolean isIpv6 = false;
+
 	public static String[] generateCmds(int type, Context context) {
+		List<String> cmds = new ArrayList<String>();
 		String cmd_type, cmd_type_dns;
+
 		switch (type) {
 		case TYPE_APPEND:
 			cmd_type = " -A ";
@@ -33,68 +40,112 @@ public class RedirectManager {
 			break;
 		}
 
+		if (type == TYPE_APPEND) {
+			cmds.add(cmd_iptables + "-N HTPROXY");
+			cmds.add(cmd_iptables + "-I OUTPUT -j HTPROXY");
+			if (isIpv6) {
+				cmds.add(cmd_ip6tables + "-N HTPROXY");
+				cmds.add(cmd_ip6tables + "-I OUTPUT -j HTPROXY");
+			}
+		}
+		if (isIpv6) {
+			cmds.add(cmd_ip6tables + cmd_type + "HTPROXY -m owner --uid-owner " +
+					 Integer.toString(android.os.Process.myUid()) + " -j RETURN");
+			cmds.add(cmd_ip6tables + cmd_type + "HTPROXY -d ::1/128 -j RETURN");
+			cmds.add(cmd_ip6tables + cmd_type + "HTPROXY -d fc00::/7 -j RETURN");
+			cmds.add(cmd_ip6tables + cmd_type + "HTPROXY -d fe80::/10 -j RETURN");
+		} else {
+			cmds.add(cmd_iptables + cmd_type + "HTPROXY -m owner --uid-owner " +
+					 Integer.toString(android.os.Process.myUid()) + " -j RETURN");
+			cmds.add(cmd_iptables + cmd_type + "HTPROXY -d 0.0.0.0/8 -j RETURN");
+			cmds.add(cmd_iptables + cmd_type + "HTPROXY -d 127.0.0.0/8 -j RETURN");
+			cmds.add(cmd_iptables + cmd_type + "HTPROXY -d 169.254.0.0/16 -j RETURN");
+			cmds.add(cmd_iptables + cmd_type + "HTPROXY -d 224.0.0.0/4 -j RETURN");
+			cmds.add(cmd_iptables + cmd_type + "HTPROXY -d 240.0.0.0/4 -j RETURN");
+		}
 		Preferences prefs = new Preferences(context);
 		Set<String> bypass_addresses = prefs.getBypassAddresses();
-		int i = 0, cmds_size = 9 + bypass_addresses.size();
-		if (prefs.getGlobalProxy()) {
-			cmds_size += 2;
-		} else {
-			Set<String> uids = prefs.getUIDs();
-			cmds_size += 2 + uids.size() * 2;
+		for (String addr : bypass_addresses) {
+			if (isIpv6 && addr.contains(":"))
+			  cmds.add(cmd_ip6tables + cmd_type + "HTPROXY -d " + addr + " -j RETURN");
+			else
+			  cmds.add(cmd_iptables + cmd_type + "HTPROXY -d " + addr + " -j RETURN");
 		}
-
-		cmds_size += (type != TYPE_CHECK) ? 2 : 0;
-		String[] cmds = new String[cmds_size];
-		if (type == TYPE_APPEND) {
-			cmds[i++] = cmd_iptables + "-N HTPROXY";
-			cmds[i++] = cmd_iptables + "-I OUTPUT -j HTPROXY";
-		}
-		cmds[i++] = cmd_iptables + cmd_type + "HTPROXY -m owner --uid-owner " +
-			Integer.toString(android.os.Process.myUid()) + " -j RETURN";
-		cmds[i++] = cmd_iptables + cmd_type + "HTPROXY -d 0.0.0.0/8 -j RETURN";
-		cmds[i++] = cmd_iptables + cmd_type + "HTPROXY -d 127.0.0.0/8 -j RETURN";
-		cmds[i++] = cmd_iptables + cmd_type + "HTPROXY -d 169.254.0.0/16 -j RETURN";
-		cmds[i++] = cmd_iptables + cmd_type + "HTPROXY -d 224.0.0.0/4 -j RETURN";
-		cmds[i++] = cmd_iptables + cmd_type + "HTPROXY -d 240.0.0.0/4 -j RETURN";
-		for (String addr : bypass_addresses)
-		  cmds[i++] = cmd_iptables + cmd_type + "HTPROXY -d " + addr + " -j RETURN";
 		if (prefs.getGlobalProxy()) {
-			cmds[i++] = cmd_iptables + cmd_type_dns + "HTPROXY -p udp --dport 53 "
-				+ " -j REDIRECT --to-port " +
-				Integer.toString(prefs.getDNSFwdPort());
-			cmds[i++] = cmd_iptables + cmd_type + "HTPROXY -p tcp " +
-				" -j REDIRECT --to-port " +
-				Integer.toString(prefs.getTProxyPort());
+			cmds.add(cmd_iptables + cmd_type_dns + "HTPROXY -p udp --dport 53 "
+					 + " -j REDIRECT --to-port " +
+					 Integer.toString(prefs.getDNSFwdPort()));
+			cmds.add(cmd_iptables + cmd_type + "HTPROXY -p tcp " +
+					 " -j REDIRECT --to-port " +
+					 Integer.toString(prefs.getTProxyPort()));
+			if (isIpv6) {
+				cmds.add(cmd_ip6tables + cmd_type_dns + "HTPROXY -p udp --dport 53 "
+						 + " -j REDIRECT --to-port " +
+						 Integer.toString(prefs.getDNSFwdPort()));
+				cmds.add(cmd_ip6tables + cmd_type + "HTPROXY -p tcp " +
+						 " -j REDIRECT --to-port " +
+						 Integer.toString(prefs.getTProxyPort()));
+			}
 		} else {
-			cmds[i++] = cmd_iptables + cmd_type_dns + "HTPROXY -d " +
-				prefs.getProxyDns1Address() + "/32 -p udp --dport 53 "
-				+ " -j REDIRECT --to-port " +
-				Integer.toString(prefs.getDNSFwdPort());
-			cmds[i++] = cmd_iptables + cmd_type_dns + "HTPROXY -d " +
-				prefs.getProxyDns2Address() + "/32 -p udp --dport 53 "
-				+ " -j REDIRECT --to-port " +
-				Integer.toString(prefs.getDNSFwdPort());
+			String addr = prefs.getProxyDns1Address();
+			if (addr.contains(":") && isIpv6)
+			  cmds.add(cmd_ip6tables + cmd_type_dns + "HTPROXY -d " +
+					   addr + " -p udp --dport 53 "
+					   + " -j REDIRECT --to-port " +
+					   Integer.toString(prefs.getDNSFwdPort()));
+			else
+			  cmds.add(cmd_iptables + cmd_type_dns + "HTPROXY -d " +
+					   addr + " -p udp --dport 53 "
+					   + " -j REDIRECT --to-port " +
+					   Integer.toString(prefs.getDNSFwdPort()));
+			addr = prefs.getProxyDns2Address();
+			if (addr.contains(":") && isIpv6)
+			  cmds.add(cmd_ip6tables + cmd_type_dns + "HTPROXY -d " +
+					   addr + " -p udp --dport 53 "
+					   + " -j REDIRECT --to-port " +
+					   Integer.toString(prefs.getDNSFwdPort()));
+			else
+			  cmds.add(cmd_iptables + cmd_type_dns + "HTPROXY -d " +
+					   addr + " -p udp --dport 53 "
+					   + " -j REDIRECT --to-port " +
+					   Integer.toString(prefs.getDNSFwdPort()));
 			Set<String> uids = prefs.getUIDs();
 			for (String uid : uids) {
 				String owner = "-m owner --uid-owner " + uid;
-				cmds[i++] = cmd_iptables + cmd_type + "HTPROXY -p tcp " + owner +
-					" -j REDIRECT --to-port " +
-					Integer.toString(prefs.getTProxyPort());
-				cmds[i++] = cmd_iptables + cmd_type + "HTPROXY -p udp --dport 53 " +
-					owner + " -j REDIRECT --to-port " +
-					Integer.toString(prefs.getDNSFwdPort());
+				cmds.add(cmd_iptables + cmd_type + "HTPROXY -p tcp " + owner +
+						 " -j REDIRECT --to-port " +
+						 Integer.toString(prefs.getTProxyPort()));
+				cmds.add(cmd_iptables + cmd_type + "HTPROXY -p udp --dport 53 " +
+						 owner + " -j REDIRECT --to-port " +
+						 Integer.toString(prefs.getDNSFwdPort()));
+				if (isIpv6) {
+					cmds.add(cmd_ip6tables + cmd_type + "HTPROXY -p tcp " + owner +
+							 " -j REDIRECT --to-port " +
+							 Integer.toString(prefs.getTProxyPort()));
+					cmds.add(cmd_ip6tables + cmd_type + "HTPROXY -p udp --dport 53 " +
+							 owner + " -j REDIRECT --to-port " +
+							 Integer.toString(prefs.getDNSFwdPort()));
+				}
 			}
 		}
-		cmds[i++] = cmd_iptables + cmd_type + "HTPROXY -d 10.0.0.0/8 -j RETURN";
-		cmds[i++] = cmd_iptables + cmd_type + "HTPROXY -d 172.16.0.0/12 -j RETURN";
-		cmds[i++] = cmd_iptables + cmd_type + "HTPROXY -d 192.168.0.0/16 -j RETURN";
-
+		if (!isIpv6) {
+			cmds.add(cmd_iptables + cmd_type + "HTPROXY -d 10.0.0.0/8 -j RETURN");
+			cmds.add(cmd_iptables + cmd_type + "HTPROXY -d 172.16.0.0/12 -j RETURN");
+			cmds.add(cmd_iptables + cmd_type + "HTPROXY -d 192.168.0.0/16 -j RETURN");
+		}
 		if (type == TYPE_DELETE) {
-			cmds[i++] = cmd_iptables + cmd_type + "OUTPUT -j HTPROXY";
-			cmds[i++] = cmd_iptables + "-X HTPROXY";
+			cmds.add(cmd_iptables + cmd_type + "OUTPUT -j HTPROXY");
+			cmds.add(cmd_iptables + "-X HTPROXY");
+			if (isIpv6) {
+				cmds.add(cmd_ip6tables + cmd_type + "OUTPUT -j HTPROXY");
+				cmds.add(cmd_ip6tables + "-X HTPROXY");
+			}
 		}
 
-		return cmds;
+		String[] cmds_ = new String[cmds.size()];
+		cmds.toArray(cmds_);
+
+		return cmds_;
 	}
 
 	public static boolean isSupported() {
@@ -102,6 +153,11 @@ public class RedirectManager {
 		  return false;
 		if (0 != SuperRunner.runCmd(cmd_iptables + "-D OUTPUT -p tcp -j REDIRECT"))
 		  return false;
+
+		if (0 == SuperRunner.runCmd(cmd_ip6tables + "-A OUTPUT -p tcp -j REDIRECT")) {
+			isIpv6 = true;
+			SuperRunner.runCmd(cmd_ip6tables + "-D OUTPUT -p tcp -j REDIRECT");
+		}
 
 		return true;
 	}
