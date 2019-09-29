902d67c6176651580dd7a1f37b30fbd4eba41e38
Heiher <r@hev.cc>
2019-09-29 11:22:36 +0800

    Preferences: Switch to VPN mode.

--- app/src/main/java/hev/htproxy/Preferences.java
+++ app/src/main/java/hev/htproxy/Preferences.java
@@ -1,4 +1,5 @@
-/* Perferences.java
+/*
+ * Perferences.java
  * Heiher <r@hev.cc>
  */
 
@@ -12,23 +13,14 @@ import android.content.SharedPreferences;
 public class Preferences
 {
 	public static final String PREFS_NAME = "Socks5Prefs";
-	public static final String SERVERS = "Servers";
-	public static final String BYPASS_ASSRESSES = "BypassAddresses";
-	public static final String GLOBAL_PROXY = "GlobalProxy";
+	public static final String CONFIGS = "Configs";
+	public static final String GLOBAL = "Global";
+	public static final String IPV4 = "Ipv4";
+	public static final String IPV6 = "Ipv6";
 	public static final String APPLICATIONS = "Applications";
-	public static final String UIDS = "UIDs";
-	public static final String EXTRA_CONFIGS = "ExtraConfigs";
-	public static final String DNS_PROXY_PATH = "DnsProxyPath";
-	public static final String ENABLE_HTPROXY = "HTProxyEnabled";
+	public static final String ENABLE = "Enable";
 
-	private static final String SERVERS_DEFAULT = "servers:\n" +
-		"  usa:\n" +
-		"    port: 80\n" +
-		"    address: 10.0.0.1\n" +
-		"    password:\n" +
-		"    protocol: *generic\n" +
-		"    weight: 1\n";
-	private static final String EXTRA_CONFIGS_DEFAULT = "protocols:\n" +
+	private static final String CONFIGS_DEFAULT = "protocols:\n" +
 		"  generic: &generic\n" +
 		"    request: 'POST /zh-cn HTTP/1.1\\r\\n" +
 				"Host: www.microsoft.com\\r\\n" +
@@ -37,7 +29,15 @@ public class Preferences
 		"    response: 'HTTP/1.1 200 OK\\r\\n" +
 				"Server: Microsoft-IIS/8.5\\r\\n" +
 				"Content-Type: application/octet-stream\\r\\n" +
-				"Connection: keep-alive\\r\\n\\r\\n'";
+				"Connection: keep-alive\\r\\n\\r\\n'\n" +
+		"\n" +
+		"servers:\n" +
+		"  usa:\n" +
+		"    port: 80\n" +
+		"    address: 10.0.0.1\n" +
+		"    password:\n" +
+		"    protocol: *generic\n" +
+		"    weight: 1\n";
 
 	private SharedPreferences prefs;
 
@@ -45,33 +45,43 @@ public class Preferences
 		prefs = context.getSharedPreferences(PREFS_NAME, Context.MODE_MULTI_PROCESS);
 	}
 
-	public String getServers() {
-		return prefs.getString(SERVERS, SERVERS_DEFAULT);
+	public String getConfigs() {
+		return prefs.getString(CONFIGS, CONFIGS_DEFAULT);
 	}
 
-	public void setServers(String servers) {
+	public void setConfigs(String extra_configs) {
 		SharedPreferences.Editor editor = prefs.edit();
-		editor.putString(SERVERS, servers);
+		editor.putString(CONFIGS, extra_configs);
 		editor.commit();
 	}
 
-	public Set<String> getBypassAddresses() {
-		return prefs.getStringSet(BYPASS_ASSRESSES, new HashSet<String>());
+	public boolean getGlobal() {
+		return prefs.getBoolean(GLOBAL, false);
 	}
 
-	public void setBypassAddresses(Set<String> addresses) {
+	public void setGlobal(boolean enable) {
 		SharedPreferences.Editor editor = prefs.edit();
-		editor.putStringSet(BYPASS_ASSRESSES, addresses);
+		editor.putBoolean(GLOBAL, enable);
 		editor.commit();
 	}
 
-	public boolean getGlobalProxy() {
-		return prefs.getBoolean(GLOBAL_PROXY, false);
+	public boolean getIpv4() {
+		return prefs.getBoolean(IPV4, true);
 	}
 
-	public void setGlobalProxy(boolean enable) {
+	public void setIpv4(boolean enable) {
 		SharedPreferences.Editor editor = prefs.edit();
-		editor.putBoolean(GLOBAL_PROXY, enable);
+		editor.putBoolean(IPV4, enable);
+		editor.commit();
+	}
+
+	public boolean getIpv6() {
+		return prefs.getBoolean(IPV6, true);
+	}
+
+	public void setIpv6(boolean enable) {
+		SharedPreferences.Editor editor = prefs.edit();
+		editor.putBoolean(IPV6, enable);
 		editor.commit();
 	}
 
@@ -85,44 +95,50 @@ public class Preferences
 		editor.commit();
 	}
 
-	public Set<String> getUIDs() {
-		return prefs.getStringSet(UIDS, new HashSet<String>());
+	public boolean getEnable() {
+		return prefs.getBoolean(ENABLE, false);
 	}
 
-	public void setUIDs(Set<String> uids) {
+	public void setEnable(boolean enable) {
 		SharedPreferences.Editor editor = prefs.edit();
-		editor.putStringSet(UIDS, uids);
+		editor.putBoolean(ENABLE, enable);
 		editor.commit();
 	}
 
-	public String getExtraConfigs() {
-		return prefs.getString(EXTRA_CONFIGS, EXTRA_CONFIGS_DEFAULT);
+	public String getTunnelName() {
+		return "tun0";
 	}
 
-	public void setExtraConfigs(String extra_configs) {
-		SharedPreferences.Editor editor = prefs.edit();
-		editor.putString(EXTRA_CONFIGS, extra_configs);
-		editor.commit();
+	public int getTunnelMtu() {
+		return 8192;
 	}
 
-	public String getDnsProxyPath() {
-		return prefs.getString(DNS_PROXY_PATH, "");
+	public String getTunnelIpv4Address() {
+		return "10.0.0.2";
 	}
 
-	public void setDnsProxyPath(String path) {
-		SharedPreferences.Editor editor = prefs.edit();
-		editor.putString(DNS_PROXY_PATH, path);
-		editor.commit();
+	public String getTunnelIpv4Gateway() {
+		return "10.0.0.1";
 	}
 
-	public boolean getHTProxyEnabled() {
-		return prefs.getBoolean(ENABLE_HTPROXY, false);
+	public int getTunnelIpv4Prefix() {
+		return 30;
 	}
 
-	public void setHTProxyEnabled(boolean enable) {
-		SharedPreferences.Editor editor = prefs.edit();
-		editor.putBoolean(ENABLE_HTPROXY, enable);
-		editor.commit();
+	public String getTunnelIpv6Address() {
+		return "fc00::2";
+	}
+
+	public String getTunnelIpv6Gateway() {
+		return "fc00::1";
+	}
+
+	public int getTunnelIpv6Prefix() {
+		return 126;
+	}
+
+	public int getTunnelDnsPort() {
+		return 53;
 	}
 
 	public String getSocks5Address() {
@@ -131,29 +147,5 @@ public class Preferences
 
 	public int getSocks5Port() {
 		return 1080;
-	}
-
-	public String getTProxyAddress() {
-		return "::";
-	}
-
-	public int getTProxyPort() {
-		return 1088;
-	}
-
-	public String getDNSFwdAddress() {
-		return "127.0.0.1";
-	}
-
-	public int getDNSFwdPort() {
-		return 5300;
-	}
-
-	public String getProxyDns1Address() {
-		return "8.8.8.8";
-	}
-
-	public String getProxyDns2Address() {
-		return "8.8.4.4";
 	}
 }
