4ddadc8ab68ca4bcdd77d19aaabfe88866943784
Heiher <r@hev.cc>
2019-09-29 11:23:47 +0800

    TProxyService: Switch to VPN mode.

--- app/src/main/java/hev/htproxy/TProxyService.java
+++ app/src/main/java/hev/htproxy/TProxyService.java
@@ -1,4 +1,5 @@
-/* TProxyService.java
+/*
+ * TProxyService.java
  * Heiher <r@hev.cc>
  */
 
@@ -7,98 +8,107 @@ package hev.htproxy;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.util.Set;
-import android.app.Service;
+import android.os.ParcelFileDescriptor;
 import android.app.Notification;
 import android.app.Notification.Builder;
 import android.app.PendingIntent;
 import android.content.Intent;
-import android.content.IntentFilter;
-import android.content.Context;
-import android.content.BroadcastReceiver;
-import android.os.IBinder;
-import android.os.Handler;
-import android.os.Message;
-import android.os.Messenger;
-import android.os.Process;
-import android.net.ConnectivityManager;
+import android.net.VpnService;
+import android.content.pm.PackageManager.NameNotFoundException;
 
-public class TProxyService extends Service {
+public class TProxyService extends VpnService {
 	private static native void Socks5StartService(String config_path);
 	private static native void Socks5StopService();
 
-	private static native void TProxyStartService(String config_path);
+	private static native void TProxyStartService(String config_path, int fd);
 	private static native void TProxyStopService();
 
-	private static native void DnsProxyStartService(String socket_path);
-	private static native void DnsProxyStopService();
-	private static native void DnsProxyResetResolver();
-	private static native void DnsProxySetResolverProxy(String dns1, String dns2);
-	private static native void DnsProxySetProxyUids(int[] uids, int last_uid);
-
-	private boolean isRunning = false;
-	private final Messenger mMessenger = new Messenger(new MessageHandler());
-
-	private final BroadcastReceiver network_state_receiver = new BroadcastReceiver() {
-		@Override
-		public void onReceive(Context context, Intent intent) {
-			resetResolver();
-		}
-	};
-
-	public class MessageHandler extends Handler {
-		public static final int TYPE_START = 1;
-		public static final int TYPE_STOP = 2;
-		public static final int TYPE_RESET_PROXY_UIDS = 3;
-
-		@Override
-		public void handleMessage(Message msg) {
-			switch (msg.what) {
-			case TYPE_START:
-				startService();
-				break;
-			case TYPE_STOP:
-				stopService();
-				break;
-			case TYPE_RESET_PROXY_UIDS:
-				resetProxyUids();
-				break;
-			default:
-				super.handleMessage(msg);
-			}
-		}
-	}
+	public static final String ACTION_CONNECT = "hev.htproxy.CONNECT";
+	public static final String ACTION_DISCONNECT = "hev.htproxy.DISCONNECT";
 
 	static {
 		System.loadLibrary("hev-socks5-client");
-		System.loadLibrary("hev-socks5-tproxy");
-		System.loadLibrary("hev-dns-proxy-jni");
+		System.loadLibrary("hev-socks5-tunnel");
 	}
+
+	private ParcelFileDescriptor tunFd = null;
 
 	@Override
 	public int onStartCommand(Intent intent, int flags, int startId) {
-		super.onStartCommand(intent, flags, startId);
-
+		if (intent != null && ACTION_DISCONNECT.equals(intent.getAction())) {
+			stopService();
+			return START_NOT_STICKY;
+		}
 		startService();
 		return START_STICKY;
 	}
 
 	@Override
 	public void onDestroy() {
-		stopService();
-
 		super.onDestroy();
 	}
 
-	public IBinder onBind(Intent intent) {
-		return mMessenger.getBinder();
+	@Override
+	public void onRevoke() {
+		stopService();
+		super.onRevoke();
 	}
 
 	public void startService() {
-		if (isRunning)
+		if (tunFd != null)
 		  return;
 
 		Preferences prefs = new Preferences(this);
+
+		/* VPN */
+		String session = new String();
+		VpnService.Builder builder = new VpnService.Builder();
+		builder.setBlocking(false);
+		builder.setMtu(prefs.getTunnelMtu());
+		if (prefs.getIpv4()) {
+			String addr = prefs.getTunnelIpv4Address();
+			int prefix = prefs.getTunnelIpv4Prefix();
+			builder.addAddress(addr, prefix);
+			builder.addRoute("0.0.0.0", 0);
+			builder.addDnsServer(prefs.getTunnelIpv4Gateway());
+			session += "IPv4";
+		}
+		if (prefs.getIpv6()) {
+			String addr = prefs.getTunnelIpv6Address();
+			int prefix = prefs.getTunnelIpv6Prefix();
+			builder.addAddress(addr, prefix);
+			builder.addRoute("::", 0);
+			builder.addDnsServer(prefs.getTunnelIpv6Gateway());
+			if (!session.isEmpty())
+			  session += " + ";
+			session += "IPv6";
+		}
+		boolean disallowSelf = true;
+		if (prefs.getGlobal()) {
+			session += "/Global";
+		} else {
+			for (String appName : prefs.getApplications()) {
+				try {
+					builder.addAllowedApplication(appName);
+					disallowSelf = false;
+				} catch (NameNotFoundException e) {
+				}
+			}
+			session += "/per-App";
+		}
+		if (disallowSelf) {
+			String selfName = getApplicationContext().getPackageName();
+			try {
+				builder.addDisallowedApplication(selfName);
+			} catch (NameNotFoundException e) {
+			}
+		}
+		builder.setSession(session);
+		tunFd = builder.establish();
+		if (tunFd == null) {
+			stopSelf();
+			return;
+		}
 
 		/* Socks5 */
 		File socks5_file = new File(getCacheDir(), "socks5.conf");
@@ -110,10 +120,9 @@ public class TProxyService extends Service {
 				"  workers: 4\n" +
 				"  port: " + prefs.getSocks5Port() + "\n" +
 				"  listen-address: '" + prefs.getSocks5Address() + "'\n" +
-				prefs.getExtraConfigs() + "\n" +
-				prefs.getServers();
+				prefs.getConfigs() + "\n";
 
-			fos.write (socks5_conf.getBytes());
+			fos.write(socks5_conf.getBytes());
 			fos.close();
 		} catch (IOException e) {
 			return;
@@ -126,36 +135,34 @@ public class TProxyService extends Service {
 			tproxy_file.createNewFile();
 			FileOutputStream fos = new FileOutputStream(tproxy_file, false);
 
-			String tproxy_conf = "main:\n" +
-				"  workers: 4\n" +
-				"socks5:\n" +
+			String tproxy_conf = "socks5:\n" +
 				"  port: " + prefs.getSocks5Port() + "\n" +
 				"  address: '" + prefs.getSocks5Address() + "'\n" +
-				"tcp:\n" +
-				"  port: " + prefs.getTProxyPort() + "\n" +
-				"  listen-address: '" + prefs.getTProxyAddress() + "'\n" +
-				"dns:\n" +
-				"  port: " + prefs.getDNSFwdPort() + "\n" +
-				"  listen-address: '" + prefs.getDNSFwdAddress() + "'\n";
+				"tunnel:\n" +
+				"  name: '" + prefs.getTunnelName() + "'\n" +
+				"  mtu: " + prefs.getTunnelMtu() + "\n" +
+				"  dns:\n" +
+				"    port: " + prefs.getTunnelDnsPort() + "\n";
 
-			fos.write (tproxy_conf.getBytes());
+			if (prefs.getIpv4()) {
+				tproxy_conf += "  ipv4:\n" +
+				"    address: '" + prefs.getTunnelIpv4Address() + "'\n" +
+				"    gateway: '" + prefs.getTunnelIpv4Gateway() + "'\n" +
+				"    prefix: " + prefs.getTunnelIpv4Prefix() + "\n";
+			}
+			if (prefs.getIpv6()) {
+				tproxy_conf += "  ipv6:\n" +
+				"    address: '" + prefs.getTunnelIpv6Address() + "'\n" +
+				"    gateway: '" + prefs.getTunnelIpv6Gateway() + "'\n" +
+				"    prefix: " + prefs.getTunnelIpv6Prefix() + "\n";
+			}
+
+			fos.write(tproxy_conf.getBytes());
 			fos.close();
 		} catch (IOException e) {
 			return;
 		}
-		TProxyStartService(tproxy_file.getAbsolutePath());
-
-		/* DnsProxy */
-		File dns_proxy_file = new File(getFilesDir(), "dnsproxyd");
-		String dns_proxy_path = dns_proxy_file.getAbsolutePath();
-		prefs.setDnsProxyPath(dns_proxy_path);
-		DnsProxySetResolverProxy(prefs.getProxyDns1Address(), prefs.getProxyDns2Address());
-		/* Reset proxy uids before startService */
-		resetProxyUids();
-		DnsProxyStartService(dns_proxy_path);
-
-		IntentFilter filter = new IntentFilter(ConnectivityManager.CONNECTIVITY_ACTION);
-		registerReceiver(network_state_receiver, filter);
+		TProxyStartService(tproxy_file.getAbsolutePath(), tunFd.getFd());
 
 		Intent i = new Intent(this, TProxyService.class);
 		PendingIntent pi = PendingIntent.getService(this, 0, i, 0);
@@ -165,23 +172,13 @@ public class TProxyService extends Service {
 			.setContentIntent(pi)
 			.build();
 		startForeground(1, notify);
-
-		/* Disable OOM */
-		setOOMAdj(-17);
-
-		isRunning = true;
 	}
 
 	public void stopService() {
-		if (!isRunning)
+		if (tunFd == null)
 		  return;
 
 		stopForeground(true);
-
-		unregisterReceiver(network_state_receiver);
-
-		/* DnsProxy */
-		DnsProxyStopService();
 
 		/* TProxy */
 		TProxyStopService();
@@ -189,32 +186,11 @@ public class TProxyService extends Service {
 		/* Socks5 */
 		Socks5StopService();
 
-		/* Enable OOM */
-		setOOMAdj(4);
-
-		isRunning = false;
-	}
-
-	public void resetResolver() {
-		DnsProxyResetResolver();
-	}
-
-	public void resetProxyUids() {
-		Preferences prefs = new Preferences(this);
-		Set<String> uids = prefs.getUIDs();
-		int i = 0, proxy_uids[] = new int[uids.size()];
-
-		for (String uid : uids) {
-			proxy_uids[i++] = Integer.parseInt(uid);
+		/* VPN */
+		try {
+			tunFd.close();
+		} catch (IOException e) {
 		}
-
-		DnsProxySetProxyUids(proxy_uids, Process.LAST_APPLICATION_UID);
-	}
-
-	private void setOOMAdj(int value) {
-		int pid = Process.myPid();
-		String cmd = String.format("echo %d > /proc/%d/oom_adj", value, pid);
-		SuperRunner.runCmd(cmd);
+		tunFd = null;
 	}
 }
-
