b42e69b95e89c88e05e6f570bb0f167b67b5d9a3
Heiher <r@hev.cc>
2019-09-29 11:24:46 +0800

    AppListActivity: Switch to VPN mode.

--- app/src/main/java/hev/htproxy/AppListActivity.java
+++ app/src/main/java/hev/htproxy/AppListActivity.java
@@ -1,4 +1,5 @@
-/* MainActivity.java
+/*
+ * AppListActivity.java
  * Heiher <r@hev.cc>
  */
 
@@ -10,48 +11,24 @@ import java.util.List;
 import java.util.Iterator;
 import java.util.Comparator;
 import java.util.Collections;
-import android.util.SparseBooleanArray;
-import android.app.ListActivity;
 import android.os.Bundle;
-import android.os.IBinder;
-import android.os.Message;
-import android.os.Messenger;
-import android.os.RemoteException;
-import android.widget.ArrayAdapter;
+import android.app.ListActivity;
+import android.view.View;
 import android.widget.CheckBox;
 import android.widget.ListView;
-import android.content.Context;
-import android.content.Intent;
-import android.content.ComponentName;
-import android.content.ServiceConnection;
+import android.util.SparseBooleanArray;
 import android.content.pm.PackageManager;
 import android.content.pm.PackageInfo;
 import android.content.pm.ApplicationInfo;
-import android.view.View;
 
 import hev.htproxy.AppArrayAdapter;
 
-public class AppListActivity extends ListActivity
-{
+public class AppListActivity extends ListActivity {
 	private Preferences prefs;
-	private Messenger mTProxyService = null;
-
-	private ServiceConnection mTProxyConnection = new ServiceConnection() {
-		public void onServiceConnected(ComponentName className, IBinder service) {
-			mTProxyService = new Messenger(service);
-		}
-
-		public void onServiceDisconnected(ComponentName className) {
-			mTProxyService = null;
-		}
-	};
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-
-		bindService(new Intent(this, TProxyService.class),
-				mTProxyConnection, Context.BIND_AUTO_CREATE);
 
 		getListView().setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
 
@@ -95,33 +72,17 @@ public class AppListActivity extends ListActivity
 		int count = list_view.getCount();
 		SparseBooleanArray sb_array = list_view.getCheckedItemPositions();
 		Set<String> applications = new HashSet<String>();
-		Set<String> uids = new HashSet<String>();
+
 		for (int i = 0; i < count; i++) {
 			if (!sb_array.get(i))
 			  continue;
 
 			PackageInfo pkginfo = adapter.getItem(i);
 			ApplicationInfo appinfo = pkginfo.applicationInfo;
-
 			applications.add(pkginfo.packageName);
-			String uid = Integer.toString(appinfo.uid);
-			if (!uids.contains(uid))
-			  uids.add(uid);
 		}
 
 		prefs.setApplications(applications);
-		prefs.setUIDs(uids);
-
-		if (mTProxyService != null) {
-			Message msg = Message.obtain(null,
-				TProxyService.MessageHandler.TYPE_RESET_PROXY_UIDS);
-			try {
-				mTProxyService.send(msg);
-			} catch (RemoteException e) {
-			}
-		}
-
-		unbindService(mTProxyConnection);
 
 		super.onDestroy();
 	}
