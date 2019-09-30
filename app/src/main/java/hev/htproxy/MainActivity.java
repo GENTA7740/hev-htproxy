3b382c4f3409992a34741dae4f1b0bb0c59b41a1
Heiher <r@hev.cc>
2019-09-30 14:54:42 +0800

    MainActivity: Switch to get configs from network.

--- app/src/main/java/hev/htproxy/MainActivity.java
+++ app/src/main/java/hev/htproxy/MainActivity.java
@@ -5,64 +5,67 @@
 
 package hev.htproxy;
 
-import java.util.Set;
-import java.util.HashSet;
-import android.app.Activity;
+import java.net.URL;
+import java.net.Authenticator;
+import java.net.PasswordAuthentication;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.io.InputStream;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import android.os.Bundle;
-import android.content.Context;
+import android.os.AsyncTask;
+import android.app.Activity;
 import android.content.Intent;
+import android.content.Context;
 import android.view.View;
-import android.widget.EditText;
-import android.widget.CheckBox;
 import android.widget.Button;
-import android.widget.Scroller;
-import android.text.method.ScrollingMovementMethod;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.Toast;
 import android.net.VpnService;
 
 public class MainActivity extends Activity implements View.OnClickListener {
 	private Preferences prefs;
-	private EditText edittext_configs;
-	private CheckBox checkbox_editable;
+	private EditText edittext_config_url;
+	private EditText edittext_username;
+	private EditText edittext_password;
 	private CheckBox checkbox_global;
 	private CheckBox checkbox_ipv4;
 	private CheckBox checkbox_ipv6;
-	private Button button_applications;
+	private Button button_apps;
+	private Button button_update;
 	private Button button_control;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 
-		setContentView(R.layout.main);
 		prefs = new Preferences(this);
+		setContentView(R.layout.main);
 
+		edittext_config_url = (EditText) findViewById(R.id.config_url);
+		edittext_username = (EditText) findViewById(R.id.username);
+		edittext_password = (EditText) findViewById(R.id.password);
+		checkbox_ipv4 = (CheckBox) findViewById(R.id.ipv4);
+		checkbox_ipv6 = (CheckBox) findViewById(R.id.ipv6);
+		checkbox_global = (CheckBox) findViewById(R.id.global);
+		button_apps = (Button) findViewById(R.id.apps);
+		button_update = (Button) findViewById(R.id.update);
+		button_control = (Button) findViewById(R.id.control);
+
+		checkbox_global.setOnClickListener(this);
+		button_apps.setOnClickListener(this);
+		button_update.setOnClickListener(this);
+		button_control.setOnClickListener(this);
+		updateUI();
+
+		/* Request VPN permission */
 		Intent intent = VpnService.prepare(MainActivity.this);
 		if (intent != null)
 		  startActivityForResult(intent, 0);
 		else
 		  onActivityResult(0, RESULT_OK, null);
-
-		edittext_configs = (EditText) findViewById(R.id.configs);
-		checkbox_editable = (CheckBox) findViewById(R.id.editable);
-		checkbox_global = (CheckBox) findViewById(R.id.global);
-		checkbox_ipv4 = (CheckBox) findViewById(R.id.ipv4);
-		checkbox_ipv6 = (CheckBox) findViewById(R.id.ipv6);
-		button_applications = (Button) findViewById(R.id.applications);
-		button_control = (Button) findViewById(R.id.control);
-
-		edittext_configs.setText(prefs.getConfigs());
-		checkbox_editable.setOnClickListener(this);
-		checkbox_editable.setChecked(false);
-		checkbox_global.setOnClickListener(this);
-		checkbox_global.setChecked(prefs.getGlobal());
-		checkbox_ipv4.setChecked(prefs.getIpv4());
-		checkbox_ipv6.setChecked(prefs.getIpv6());
-		button_applications.setOnClickListener(this);
-		button_control.setOnClickListener(this);
-		if (prefs.getEnable())
-		  button_control.setText(R.string.control_disable);
-
-		setEditable();
 	}
 
 	@Override
@@ -79,46 +82,128 @@ public class MainActivity extends Activity implements View.OnClickListener {
 		}
 	}
 
+	@Override
 	public void onClick(View view) {
-		if (view == checkbox_editable) {
-			setEditable();
-		} else if (view == checkbox_global) {
-			button_applications.setEnabled(!checkbox_global.isChecked());
-		} else if (view == button_applications) {
+		if (view == checkbox_global) {
+			savePrefs();
+			updateUI();
+		} else if (view == button_apps) {
 			startActivity(new Intent(this, AppListActivity.class));
+		} else if (view == button_update) {
+			savePrefs();
+			updateConfigs();
 		} else if (view == button_control) {
 			boolean isEnable = prefs.getEnable();
 			Intent intent = new Intent(this, TProxyService.class);
-			if (isEnable) {
-				button_control.setText(R.string.control_enable);
-				startService(intent.setAction(TProxyService.ACTION_DISCONNECT));
-			} else {
-				button_control.setText(R.string.control_disable);
-				savePrefs();
-				startService(intent.setAction(TProxyService.ACTION_CONNECT));
-			}
+			if (isEnable)
+			  startService(intent.setAction(TProxyService.ACTION_DISCONNECT));
+			else
+			  startService(intent.setAction(TProxyService.ACTION_CONNECT));
 			prefs.setEnable(!isEnable);
-			setEditable();
+			savePrefs();
+			updateUI();
 		}
 	}
 
-	private void setEditable() {
-		boolean enable = prefs.getEnable();
-		boolean editable = checkbox_editable.isChecked() && !enable;
-		edittext_configs.setEnabled(editable);
+	private void updateUI() {
+		edittext_config_url.setText(prefs.getConfigUrl());
+		edittext_username.setText(prefs.getUsername());
+		edittext_password.setText(prefs.getPassword());
+		checkbox_ipv4.setChecked(prefs.getIpv4());
+		checkbox_ipv6.setChecked(prefs.getIpv6());
+		checkbox_global.setChecked(prefs.getGlobal());
+
+		boolean editable = !prefs.getEnable();
+		edittext_config_url.setEnabled(editable);
+		edittext_username.setEnabled(editable);
+		edittext_password.setEnabled(editable);
 		checkbox_global.setEnabled(editable);
 		checkbox_ipv4.setEnabled(editable);
 		checkbox_ipv6.setEnabled(editable);
-		button_applications.setEnabled(!checkbox_global.isChecked() && editable);
-		checkbox_editable.setEnabled(!enable);
+		button_apps.setEnabled(editable && !prefs.getGlobal());
+		button_update.setEnabled(editable);
+
+		if (editable)
+		  button_control.setText(R.string.control_enable);
+		else
+		  button_control.setText(R.string.control_disable);
+		button_control.setEnabled(!prefs.getConfigs().isEmpty());
 	}
 
 	private void savePrefs() {
-		prefs.setConfigs(edittext_configs.getText().toString());
-		prefs.setGlobal(checkbox_global.isChecked());
+		prefs.setConfigUrl(edittext_config_url.getText().toString());
+		prefs.setUsername(edittext_username.getText().toString());
+		prefs.setPassword(edittext_password.getText().toString());
 		if (!checkbox_ipv4.isChecked() && !checkbox_ipv4.isChecked())
 		  checkbox_ipv4.setChecked(prefs.getIpv4());
 		prefs.setIpv4(checkbox_ipv4.isChecked());
 		prefs.setIpv6(checkbox_ipv6.isChecked());
+		prefs.setGlobal(checkbox_global.isChecked());
+	}
+
+	private void updateConfigs() {
+		Authenticator.setDefault(new Authenticator() {
+			protected PasswordAuthentication getPasswordAuthentication() {
+				String user = prefs.getUsername();
+				String pass = prefs.getPassword();
+				return new PasswordAuthentication(user, pass.toCharArray());
+			}
+		});
+
+		AsyncTaskRunner task = new AsyncTaskRunner();
+		task.execute();
+		Toast.makeText(this, "Updating configs...", Toast.LENGTH_LONG).show();
+	}
+
+	private class AsyncTaskRunner extends AsyncTask<Void, Void, Boolean> {
+		@Override
+		protected Boolean doInBackground(Void... params) {
+			Preferences prefs = new Preferences(getApplicationContext());
+			boolean res = false;
+			URL url;
+
+			try {
+				url = new URL(prefs.getConfigUrl());
+			} catch (MalformedURLException e) {
+				return false;
+			}
+
+			try {
+				HttpURLConnection conn = (HttpURLConnection)url.openConnection();
+				conn.setConnectTimeout(5000);
+				conn.setReadTimeout(5000);
+				if (conn.getResponseCode() == 200) {
+					int s, size = 0, total = 16384;
+					byte[] content = new byte[total + 1];
+					InputStream istream = conn.getInputStream();
+					for (; (total - size) > 0; size += s) {
+						s = istream.read(content, size, total - size);
+						if (s <= 0)
+						  break;
+					}
+					if (size > 0) {
+						prefs.setConfigs(new String(content, 0, size, StandardCharsets.UTF_8));
+						res = true;
+					}
+					istream.close();
+				}
+				conn.disconnect();
+			} catch (IOException e) {
+				return false;
+			}
+
+			return res;
+		}
+
+		@Override
+		protected void onPostExecute (Boolean success) {
+			Context context = getApplicationContext();
+			if (!success) {
+				Toast.makeText(context, "Update configs failed!", Toast.LENGTH_SHORT).show();
+				return;
+			}
+			updateUI();
+			Toast.makeText(context, "Update configs successful!", Toast.LENGTH_SHORT).show();
+		}
 	}
 }
